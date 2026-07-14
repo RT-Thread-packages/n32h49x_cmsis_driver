216bb8d7128fb3a5ec3ef243a60591af641630bc
OnlyoutzZ <17393117531@163.com>
2026-07-14 11:13:56 +0800

    上传n32h49x_cmsis_driver

---
+++ device/n32h49x_conf.h
@@ -0,0 +1,91 @@
+/**
+*     Copyright (c) 2025, Nsing Technologies Inc.
+* 
+*     All rights reserved.
+*
+*     This software is the exclusive property of Nsing Technologies Inc. (Hereinafter 
+* referred to as Nsing). This software, and the product of Nsing described herein 
+* (Hereinafter referred to as the Product) are owned by Nsing under the laws and treaties
+* of the People's Republic of China and other applicable jurisdictions worldwide.
+*
+*     Nsing does not grant any license under its patents, copyrights, trademarks, or other 
+* intellectual property rights. Names and brands of third party may be mentioned or referred 
+* thereto (if any) for identification purposes only.
+*
+*     Nsing reserves the right to make changes, corrections, enhancements, modifications, and 
+* improvements to this software at any time without notice. Please contact Nsing and obtain 
+* the latest version of this software before placing orders.
+
+*     Although Nsing has attempted to provide accurate and reliable information, Nsing assumes 
+* no responsibility for the accuracy and reliability of this software.
+* 
+*     It is the responsibility of the user of this software to properly design, program, and test 
+* the functionality and safety of any application made of this information and any resulting product. 
+* In no event shall Nsing be liable for any direct, indirect, incidental, special,exemplary, or 
+* consequential damages arising in any way out of the use of this software or the Product.
+*
+*     Nsing Products are neither intended nor warranted for usage in systems or equipment, any
+* malfunction or failure of which may cause loss of human life, bodily injury or severe property 
+* damage. Such applications are deemed, "Insecure Usage".
+*
+*     All Insecure Usage shall be made at user's risk. User shall indemnify Nsing and hold Nsing 
+* harmless from and against all claims, costs, damages, and other liabilities, arising from or related 
+* to any customer's Insecure Usage.
+
+*     Any express or implied warranty with regard to this software or the Product, including,but not 
+* limited to, the warranties of merchantability, fitness for a particular purpose and non-infringement
+* are disclaimed to the fullest extent permitted by law.
+
+*     Unless otherwise explicitly permitted by Nsing, anyone may not duplicate, modify, transcribe
+* or otherwise distribute this software for any purposes, in whole or in part.
+*
+*     Nsing products and technologies shall not be used for or incorporated into any products or systems
+* whose manufacture, use, or sale is prohibited under any applicable domestic or foreign laws or regulations. 
+* User shall comply with any applicable export control laws and regulations promulgated and administered by 
+* the governments of any countries asserting jurisdiction over the parties or transactions.
+**/
+
+/**
+*\*\file n32h49x_conf.h
+*\*\author Nsing
+*\*\version v1.0.0
+*\*\copyright Copyright (c) 2025, Nsing Technologies Inc. All rights reserved.
+**/ 
+#ifndef __N32H49X_CONF_H__
+#define __N32H49X_CONF_H__
+
+/* Uncomment/Comment the line below to enable/disable peripheral header file inclusion */
+
+#include "n32h49x_adc.h"
+#include "n32h49x_cordic.h"
+#include "n32h49x_crc.h"
+#include "n32h49x_dac.h"
+#include "n32h49x_dbg.h"
+#include "n32h49x_dma.h"
+#include "n32h49x_dsmu.h"
+#include "n32h49x_dvp.h"
+#include "n32h49x_eth.h"
+#include "n32h49x_exti.h"
+#include "n32h49x_fdcan.h"
+#include "n32h49x_femc.h"
+#include "n32h49x_flash.h"
+#include "n32h49x_gpio.h"
+#include "n32h49x_i2c.h"
+#include "n32h49x_iwdg.h"
+#include "n32h49x_lptim.h"
+#include "n32h49x_pwr.h"
+#include "n32h49x_rcc.h"
+#include "n32h49x_rtc.h"
+#include "n32h49x_sdio.h"
+#include "n32h49x_spi.h"
+#include "n32h49x_tim.h"
+#include "n32h49x_usart.h"
+#include "n32h49x_vrefbuf.h"
+#include "n32h49x_wwdg.h"
+#include "n32h49x_xspi.h"
+#include "n32h49x_xspi_v2.h"
+
+#include "misc.h" /* High level functions for NVIC and SysTick (add-on to CMSIS functions) */
+
+
+#endif /* __N32H49X_CONF_H__ */
